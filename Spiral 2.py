4b6168bd954efcd7a170eb4e8d7a38221b7b52be
Riaque <74700733+Rifaque@users.noreply.github.com>
2023-01-12 08:20:44 +0530

    Add files via upload

---
+++ Spiral 2.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.speed(0)
+colors = ['blue', 'black', 'blue', 'black', 'blue', 'black']
+t = turtle.Pen()
+turtle.bgcolor('white')
+for x in range(360):
+    t.pencolor(colors[x%6])
+    t.width(x//100 + 1)
+    t.forward(x)
+    t.right(59)
